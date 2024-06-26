2de714fd8bd70534e6260dd4af4fda54d1017d9a
tk-yoshimura <yoshimura8192@gmail.com>
2024-06-26 20:39:18 +0900

    add: fp128

---
+++ MapAiryDistributionFP128/ExMath.cs
@@ -0,0 +1,17 @@
+﻿using MultiPrecision;
+
+namespace MapAiryDistributionFP128 {
+    internal static class ExMath {
+
+        public static MultiPrecision<Pow2.N4> Square(MultiPrecision<Pow2.N4> x) => x * x;
+        public static MultiPrecision<Pow2.N4> Cube(MultiPrecision<Pow2.N4> x) => x * x * x;
+
+        public static MultiPrecision<Pow2.N4> Pow3d2(MultiPrecision<Pow2.N4> x) {
+            return MultiPrecision<Pow2.N4>.Sqrt(Cube(x));
+        }
+
+        public static MultiPrecision<Pow2.N4> Pow2d3(MultiPrecision<Pow2.N4> x) {
+            return MultiPrecision<Pow2.N4>.Cbrt(Square(x));
+        }
+    }
+}
